b8478e249a22754c2695374eaf4b30f75b551cda
LoveMary <nassim.bentayeb@yahoo.com>
2018-05-17 10:28:48 +0200

    ADD scanner to scannerPage

--- src/pages/scanner/scanner.ts
+++ src/pages/scanner/scanner.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner';
+
 
 @Component({
   selector: 'page-scanner',
   templateUrl: 'scanner.html'
 })
 export class ScannerPage {
+  public buttonText : string;
+  public loading: boolean;
+  public result: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner, private toastcontroller : ToastController) {
 
   }
+  
+  ionViewDidLoad() {
+    this.buttonText = "Scan";
+    this.loading =false;
+  }
+
+ scanQRC(){
+   const options: BarcodeScannerOptions = {
+    torchOn: false
+   }
+   
+   this.barcodeScanner.scan(options)
+    .then(res =>{ 
+      this.result = res.text
+      ;})
+    .catch(error =>{
+      this.toastcontroller.create({
+        message: error.message
+      })
+      .present();
+    })
+
+ };
 
 }
